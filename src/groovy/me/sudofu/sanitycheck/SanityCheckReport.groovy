e973d93cb75acb5283c89d90b89300ed7fc3264d
Aaron Brown <brown.aaron.lloyd@gmail.com>
2014-01-12 20:11:40 -0600

    JavaDoc SanityCheckReport.

--- src/groovy/me/sudofu/sanitycheck/SanityCheckReport.groovy
+++ src/groovy/me/sudofu/sanitycheck/SanityCheckReport.groovy
@@ -14,22 +14,66 @@
  */
 package me.sudofu.sanitycheck
 
+/**
+ * Reporting structure for {@link me.sudofu.sanitycheck.SanityChecker SanityCheckers}.
+ *
+ * @author  Aaron Brown
+ */
 class SanityCheckReport {
+
+    /**
+     * Enumeration for possible outcomes of a sanity check.
+     */
     public enum Result {
         PASS,
         FAIL
     }
 
+    /**
+     * The internal report structure.
+     */
     protected List<Map> report
 
+    /**
+     * Constructs a <b><code>SanityCheckReport</code></b>.
+     */
     public SanityCheckReport() {
         this.report = []
     }
 
+    /**
+     * Prevent setting.
+     *
+     * @param   arg
+     *
+     * Argument.
+     */
     private void setReport(List<Map> arg) {
 
     }
 
+    /**
+     * Use when a sanity check has passed.
+     *
+     * @param   name
+     *
+     * The name of the entity being checked (for example, the name of
+     * the parameter).
+     *
+     * @param   classification
+     *
+     * The classification of the entity that is being checked
+     * (for example, parameter).
+     *
+     * @param   check
+     *
+     * The check that was performed (for example, <b>isNotNull</b>).
+     *
+     * @param   checkDesscription
+     *
+     * A succinct description of the check, or its intended purpose
+     * (for example, <i>Must not be <code>null</code></i>).
+     */
     public void pass(String name, String classification, String check, String checkDescription) {
         report << [
             result: Result.PASS,
@@ -40,6 +84,28 @@ class SanityCheckReport {
         ]
     }
 
+    /**
+     * Use when a sanity check has failed.
+     *
+     * @param   name
+     *
+     * The name of the entity being checked (for example, the name of
+     * the parameter).
+     *
+     * @param   classification
+     *
+     * The classification of the entity that is being checked
+     * (for example, parameter).
+     *
+     * @param   check
+     *
+     * The check that was performed (for example, <b>isNotNull</b>).
+     *
+     * @param   checkDesscription
+     *
+     * A succinct description of the check, or its intended purpose
+     * (for example, <i>Must not be <code>null</code></i>).
+     */
     public void fail(String name, String classification, String check, String checkDescription) {
         report << [
             result: Result.FAIL,
@@ -50,26 +116,152 @@ class SanityCheckReport {
         ]
     }
 
+    /**
+     * Determine if the report has instances of sanity checks that have
+     * passed.
+     *
+     * @return
+     *
+     * <code>true</code> if at least one sanity check has passed;
+     * <code>false</code> otherwise.
+     */
     public boolean hasPasses() {
         return report.any { it.result == Result.PASS }
     }
 
+    /**
+     * Determine if the report has instances of sanity checks that have
+     * failed.
+     *
+     * @return
+     *
+     * <code>true</code> if at least one sanity check has failed;
+     * <code>false</code> otherwise.
+     */
     public boolean hasFailures() {
         return report.any { it.result == Result.FAIL }
     }
 
+    /**
+     * Count the number of sanity check passes.
+     *
+     * <p>
+     * In practice, this number is unpredictable as some sanity checks
+     * may have implicit sanity checks. For example, most sanity checks
+     * will perform a check to ensure the entity is not <code>null</code>
+     * before performing the actual check, which would mean for that
+     * one check, <b>2</b> would be the total count (if both passed).
+     * </p>
+     *
+     * @return
+     *
+     * The number of passed sanity check entries; <code>0</code> (zero)
+     * otherwise.
+     */
     public int countPasses() {
         return getPasses().size()
     }
 
+    /**
+     * Count the number of sanity check failures.
+     *
+     * <p>
+     * In practice, this number is unpredictable as some sanity checks
+     * may have implicit sanity checks. For example, most sanity checks
+     * will perform a check to ensure the entity is not <code>null</code>
+     * before performing the actual check, which would mean for that
+     * one check, <b>2</b> would be the total count (if both failed).
+     * </p>
+     *
+     * @return
+     *
+     * The number of failed sanity check entries; <code>0</code> (zero)
+     * otherwise.
+     */
     public int countFailures() {
         return getFailures().size()
     }
 
+    /**
+     * Retrieve the entries for sanity checks that have passed.
+     *
+     * <p>
+     * <table>
+     *  <tr>
+     *   <th>Key</th>
+     *   <th>Description</th>
+     *  </tr>
+     *  <tr>
+     *   <td>result</td>
+     *   <td>Whether or not the check passed or failed.</td>
+     *  </tr>
+     *  <tr>
+     *   <td>name</td>
+     *   <td>The name of the entity checked against.</td>
+     *  </tr>
+     *  <tr>
+     *   <td>classification</td>
+     *   <td>The classification of the entity checked against.</td>
+     *  </tr>
+     *  <tr>
+     *   <td>check</td>
+     *   <td>A symbol / name of the check performed on the entity.</td>
+     *  </tr>
+     *  <tr>
+     *   <td>checkDescription</td>
+     *   <td>A succinct description or intended purpose of the check.</td>
+     *  </tr>
+     * </table>
+     * </p>
+     *
+     * @return
+     *
+     * A <code>List</code> of those checks that have passed, described
+     * in <code>Map</code>s; an empty <code>List</code> if no checks
+     * were performed, or no passes occured.
+     */
     public List<Map> getPasses() {
         return report.findAll { it.result == Result.PASS }
     }
 
+    /**
+     * Retrieve the entries for sanity checks that have failed.
+     *
+     * <p>
+     * <table>
+     *  <tr>
+     *   <th>Key</th>
+     *   <th>Description</th>
+     *  </tr>
+     *  <tr>
+     *   <td>result</td>
+     *   <td>Whether or not the check passed or failed.</td>
+     *  </tr>
+     *  <tr>
+     *   <td>name</td>
+     *   <td>The name of the entity checked against.</td>
+     *  </tr>
+     *  <tr>
+     *   <td>classification</td>
+     *   <td>The classification of the entity checked against.</td>
+     *  </tr>
+     *  <tr>
+     *   <td>check</td>
+     *   <td>A symbol / name of the check performed on the entity.</td>
+     *  </tr>
+     *  <tr>
+     *   <td>checkDescription</td>
+     *   <td>A succinct description or intended purpose of the check.</td>
+     *  </tr>
+     * </table>
+     * </p>
+     *
+     * @return
+     *
+     * A <code>List</code> of those checks that have failed, described
+     * in <code>Map</code>s; an empty <code>List</code> if no checks
+     * were performed, or no failures occured.
+     */
     public List<Map> getFailures() {
         return report.findAll { it.result == Result.FAIL }
     }
