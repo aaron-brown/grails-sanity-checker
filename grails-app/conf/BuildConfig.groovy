c01197a7ff6a3e72fdd7320347a56402e00ef635
Aaron Brown <brown.aaron.lloyd@gmail.com>
2014-01-26 22:06:33 -0600

    Updating buildconfig.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -27,9 +27,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:2.0.3",
-              ":rest-client-builder:1.0.2") {
+        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
             export = false
         }
     }
